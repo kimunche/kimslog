a584c9c98f1072b726ef94449d4b6a6c3230e640
kimunche <angela_id@naver.com>
2023-04-18 20:50:00 +0900

    postService test case 작성

---
+++ src/test/java/com/kimslog/service/PostServiceTest.java
@@ -0,0 +1,44 @@
+package com.kimslog.service;
+
+import com.kimslog.domain.Post;
+import com.kimslog.repository.PostRepository;
+import com.kimslog.request.PostCreate;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class PostServiceTest {
+
+    @Autowired
+    PostService postService;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Test
+    @DisplayName("글 작성")
+    void writeContent(){
+        //given
+        PostCreate postCreate = PostCreate.builder()
+                .title("제목")
+                .content("내용")
+                .build();
+
+        //when
+        postService.write(postCreate);
+
+        //then
+        assertEquals(1L, postRepository.count());
+
+        Post post = postRepository.findAll().get(0);
+        assertEquals("제목", post.getTitle());
+        assertEquals("내용", post.getContent());
+    }
+}
